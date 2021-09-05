580fb1b819afebb0da6e8f7485908de790e828ea
youny <youny1118@gmail.com>
2021-09-05 10:48:54 +0800

    最短无序连续子数组

--- src/main/java/structure/README.md
+++ src/main/java/structure/README.md
@@ -1,14 +1,45 @@
 # 第一难
-金蝉遭贬
-> 
+
+
+
+这里希望加一个什么GitHub的一个icon来显示一下 **金蝉遭贬**
+
+> 如来想到了自己顽劣的弟子—**金蝉子**，便罚其轮回转世一次, 降生到**东土大唐**，担负起**取经**的重任
+
+
 
 ## 数据结构
+
+> 数据结构存储方式：**数组存储和链表存储**
+>
+> 数据结构有很多种类，但它们存在的目的就是在不同的应用场景，尽可能**高效地增删改查**
+
+
+
+### 1. 数组
+
+> **线性**数据结构，一组**连续的内存空间**，来**存储**一组具有**相同类型**的数据
+
+![arrayProto](/Users/beyond/yy/文档/images/algorithm/arrayProto.png)
+
+数组具有连续的空间并且占有相同字节的数据，因此每个节点的数据可以通过 `a[i]_address = base_address + i * data_type_size `
+
+
+
+**题目**
+
+* <a style='text-decoration: none;' src= 'https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/'>最短无序连续子数组</a>
+*
+
+
+
+### 2. 链表
+
+
+
+
 
 ### 栈
 
 
-### 堆
 
-### 队列
-
-### 树
--- src/main/java/structure/array/Array.java
+++ src/main/java/structure/array/Array.java
@@ -21,17 +21,28 @@ public class Array {
     public void findUnsortedSubarray(){
 
         int[] nums = {2, 6, 4, 8, 10, 9, 15};
-        assertEquals(5,findUnsortedSubarrayOne(nums));
         int [] nums2 = {1,2,3,4};
-        assertEquals(0,findUnsortedSubarrayOne(nums2));
         int [] nums3 = {1};
+        assertEquals(5,findUnsortedSubarrayOne(nums));
+        assertEquals(0,findUnsortedSubarrayOne(nums2));
         assertEquals(0,findUnsortedSubarrayOne(nums3));
+
+        assertEquals(5,findUnsortedSubarrayTwo(nums));
+        assertEquals(0,findUnsortedSubarrayTwo(nums2));
+        assertEquals(0,findUnsortedSubarrayTwo(nums3));
 
     }
 
+    /**
+     * 思路：也就是找出 三个子数组 A B C
+     *  满足 A，C里面所有的元素有序 而且B里面的所有元素大于A小于C并且无序
+     *  双指针
+     * @param nums
+     * @return
+     */
     public int findUnsortedSubarrayOne(int[] nums){
         // 查询是否已经排好序
-        boolean flag = true;
+        boolean flag = false;
         for (int i = 1; i < nums.length; i++) {
             if(nums[i-1] > nums[i]){
                 flag = true;
@@ -44,13 +55,42 @@ public class Array {
         System.arraycopy(nums,0,copyArray,0,nums.length);
         Arrays.sort(copyArray);
         int left = 0;
-        while(left < nums.length && nums[left] == copyArray[left]){
+        while(nums[left] == copyArray[left]){
             left++;
         }
         int right = nums.length;
-        while(right > left && nums[right-1] == copyArray[right-1]){
+        while(nums[right-1] == copyArray[right-1]){
             right--;
         }
         return right - left;
+    }
+
+
+    /**
+     * 双指针
+     * 左右指针什么时候移动？
+     * 右指针只需要右边是最大数,因此只要求出左边的最大数即可
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarrayTwo(int[] nums){
+
+        int n = nums.length;
+        int max = Integer.MIN_VALUE,right = -1;
+        int min = Integer.MAX_VALUE,left = -1;
+
+        for (int i = 0; i < n; i++) {
+            if(max > nums[i]){
+                right = i;
+            }else{
+                max = nums[i];
+            }
+            if(nums[n-1-i] < min){
+                min = nums[n-1-i];
+            }else{
+                left = n -1-i;
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
     }
 }
